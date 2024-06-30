c8c39d7fb826320b35950ee894750427d93d074b
Ugochi Ukaegbu <ugochiukaegbu19@gmail.com>
2024-06-30 13:57:25 +0100

    variables in the resources

---
+++ website-deploy-azureVm-task/terraform-root-modules/variable.tf
@@ -0,0 +1,9 @@
+variable "name" {}
+variable "region" {}
+variable "cidr_block" {}
+variable "subnets" {}
+variable "allocation_method" {}
+variable "source_file_path" {}
+variable "destination_file_path" {}
+variable "subscription_id" {}
+
