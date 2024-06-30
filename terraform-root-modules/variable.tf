150ea98171c85860763802d18e0984f6cd2debcf
zenitugo <ugochiukaegbu19@gmail.com>
2024-06-30 18:35:39 +0100

    variables in the resources

---
+++ terraform-root-modules/variable.tf
@@ -0,0 +1,11 @@
+variable "name" {}
+variable "region" {}
+variable "cidr_block" {}
+variable "subnets" {}
+variable "allocation_method" {}
+variable "source_file_path" {}
+variable "destination_file_path" {}
+variable "subscription_id" {}
+variable "admin_username" {}
+variable "admin_password" {}
+
